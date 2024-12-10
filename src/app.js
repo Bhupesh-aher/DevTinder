66452e3ad23adf65bd9b31a512ec791553c703c6
Bhupesh-aher <aherbhupesh18@gmail.com>
2024-12-10 20:26:16 +0530

    multi request handler/functions

--- src/app.js
+++ src/app.js
@@ -4,135 +4,55 @@ const app = express();
 
 
 
+app.use("/user", 
+    (req, res, next) => {
+    // request handler 
+    console.log("request handler!!!");
+    // res.send("1st response")
+    next();
 
-
-
-// how can you get query param in my req function (route handler or controller)
-// we can read query parameter like this -
-// /user?userId=3
-// how can i read this
-// by doing this - 
-// console.log(req.query); 
-// { userId: '3' }
-
-// app.get("/user", (req, res) => {
-//         console.log(req.query);
-        
-//         res.send({
-//             firstName: "gulu",
-//             lastName: "singh"
-//         })
-//     })
-
-
-
-// dynamic id in url
-// user/102 or user/225
-// by doing like this 
-///user/:userId", (req, res) => {
-//console.log(req.params);
-//{ userId: '102' }
-// : this means it's a dynamic route
-
-app.get("/user/:userId", (req, res) => {
-    console.log(req.params);
+}, (req, res, next) => {
+    console.log("request handler 2 !!!!!");
+    next();
     
-    res.send({
-        firstName: "gulu",
-        lastName: "singh"
-    })
+}, (req, res, next) => {
+    console.log("request handler 3 !!!!!");
+    // res.send("3rd response")
+    next()
+    
+}, (req, res, next) => {
+    console.log("request handler 4 !!!!!");
+    // res.send("4th response")
+    next();
+    
+}, (req, res, next) => {
+    console.log("request handler 5 !!!!!");
+    // res.send("5th response")
+    next();
+    
 })
 
+// in this way we can create multiple request handlers for one route
 
-// if we put ? after b then b is optional
-// these will work - /abc   /ac     
-// these won't work - /ab   /abww / abbbc    
-// app.get("/ab?c", (req, res) => {
-//     res.send({
-//         firstName: "gulu",
-//         lastName: "singh"
-//     })
-// })
+// and we can do the same thing for all the http methods as well 
+// here we done this with app.use 
+// but we can do all this with app.get, post, patch, delete and other methods as well 
 
+// app.use("/user", rh1, rh2, rh3, rh4, rh5)
+// app.use("/user", [rh1, rh2, rh3, rh4, rh5])
+// app.use("/user", rh1, [rh2, rh3], rh4, rh5)
+// app.use("/user", rh1, [rh2], rh3, rh4, rh5)
 
-// this means bc is optional
-// these will work - /abcd   /ad     
-// these won't work - /abc   /abd
-// app.get("/a(bc)?d", (req, res) => {
-//     res.send({
-//         firstName: "gulu",
-//         lastName: "singh"
-//     })
-// })
-
-
-
-
-
-
-
-// if we put + after b then b can be written as many times you we want
-// these will work - /abc   /abbbbbc     
-// these won't work - /abeeec   /abbbbccc  
-
-// app.get("/ab+c", (req, res) => {
-//     res.send({
-//         firstName: "gulu",
-//         lastName: "singh"
-//     })
-// })
-
-
-
-// app.get("/a(bc)+d", (req, res) => {
-//     res.send({
-//         firstName: "gulu",
-//         lastName: "singh"
-//     })
-// })
-
-
-
-
-// if we put * after ab and before cd then between ab and cd we can put anything
-// these will work - /abcd   /ab25526cd  / abgdge344cd   /abbcd
-// these won't work - /ae13cd   /abbd  
-
-// app.get("/ab*cd", (req, res) => {
-//     res.send({
-//         firstName: "gulu",
-//         lastName: "singh"
-//     })
-// })
-
-
-
-
-// regex 
-// this means in my path if "a" letter is there then path will work
-// these will work - /a   /cab /ab / ca
-// these won't work - /b       /bc
-
-// app.get(/a/, (req, res) => {
-//     res.send({
-//         firstName: "gulu",
-//         lastName: "singh"
-//     })
-// })
-
-
-// * means anything in starting and in end fly 
-// these will work - /fly /butterfly   /dragonfly
-// these won't work - /butterfly1    /fly3
-
-// app.get(/.*fly$/, (req, res) => {
-//     res.send({
-//         firstName: "gulu",
-//         lastName: "singh"
-//     }) 
-// })
-
+// they will give the same output 
+// there will be no imapct on output
 
 app.listen(7777, () => {
     console.log("server is successfully listening on port 7777...");
 });
+
+
+
+
+
+
+
