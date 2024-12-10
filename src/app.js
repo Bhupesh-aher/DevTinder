282b7cbf99a06903ddda214b5970e75242f5ea3d
Bhupesh-aher <aherbhupesh18@gmail.com>
2024-12-10 21:57:44 +0530

    multiple route handlers/functions

--- src/app.js
+++ src/app.js
@@ -4,9 +4,11 @@ const app = express();
 
 
 
+
+
 app.use("/user", 
     (req, res, next) => {
-    // request handler 
+    // Route handler 
     console.log("request handler!!!");
     // res.send("1st response")
     next();
@@ -32,10 +34,27 @@ app.use("/user",
     
 })
 
-// in this way we can create multiple request handlers for one route
+
+
+
+
+
+
+//  "/user",   this is called as router/route
+// and this is router handler - this function is known as route handler
+// (req, res, next) => {
+        // route handler
+//     console.log("request handler 2 !!!!!");
+//     next();
+    
+// }
+
+// one route can also have multuiple route handlers
+// in this way we can create multiple route handlers for one route
+
 
 // and we can do the same thing for all the http methods as well 
-// here we done this with app.use 
+// here we did this with app.use 
 // but we can do all this with app.get, post, patch, delete and other methods as well 
 
 // app.use("/user", rh1, rh2, rh3, rh4, rh5)
@@ -44,7 +63,7 @@ app.use("/user",
 // app.use("/user", rh1, [rh2], rh3, rh4, rh5)
 
 // they will give the same output 
-// there will be no imapct on output
+// there will be no impact on output they will behave in same way as they use to
 
 app.listen(7777, () => {
     console.log("server is successfully listening on port 7777...");
