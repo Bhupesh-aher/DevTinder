4c549d69ca0f82b42bbea9bb8b8152f10a8df031
Bhupesh-aher <aherbhupesh18@gmail.com>
2024-12-11 13:05:05 +0530

    wildcard error handlong

--- src/app.js
+++ src/app.js
@@ -2,57 +2,39 @@ const express = require("express")
 
 const app = express();
 
-
-
-// app.get("/admin/getAllData", (req, res) => {
-//     const tokens = "xyzededded";
-//     const isAdminAuthorized = tokens === "xyz";
-//     if(isAdminAuthorized){
-//         res.send("all data send")
-//     }
-//     else{  
-//         res.status(401).send("unauthorized request");
-//     }
-// })
-
-
-// app.get("/admin/deleteUser", (req, res) => {
-//     const tokens = "xyzwsss";
-//     const isAdminAuthorized = tokens === "xyz";
-//     if(isAdminAuthorized){
-//         res.send("deletd a user")
-//     }
-//     else{
-//         res.status(401).send("unauthorized request");
-//     }
-    
-// })
-
-const {adminAuth, userAuth} = require("./middlewares/auth")
-
-
-// handle auth middleware for all GET, POST,... requests
-app.use("/admin", adminAuth)
-// app.use("/user", userAuth);
-
-app.get("/user", userAuth,(req, res) => {
-    console.log("user handler");
-    
-    res.send("user is sent")
+app.use("/", (err, req, res, next) => {
+    if(err) {
+        console.log("error 2");
+        
+        res.status(500).send("something went wrong 2")
+    }
 })
 
-app.get("/admin/getAllData", (req, res) => {
-    console.log("get all data handler");
-    
-    res.send("all data sent")
+app.get("/getUserData" , (req, res, next) => {
+    // const flag = false;
+    // try{
+        // if(flag){
+            throw new error("jsbjb")
+            res.send("user data is sent")
+           
+            
+        // }
+        // else {
+        // }
+    // }
+    // catch(err) {
+    //     res.status(500).send("contact support team")
+    // }
 })
 
-app.get("/admin/deleteUser", (req, res) => {
-    console.log("delete data handler");
 
-    res.send("deleted a user")
+app.use("/", (err, req, res, next) => {
+    if(err) {
+        console.log("error");
+        
+        res.status(500).send("something went wrong")
+    }
 })
-
 
 
 
