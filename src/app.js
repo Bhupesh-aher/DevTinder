30e059ad5fa27f3059caa047d294e81da2bb83e0
Bhupesh-aher <aherbhupesh18@gmail.com>
2024-12-13 19:55:33 +0530

    build user api & feed api

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const connectDB = require("./config/database")
 
 app.use(express.json());
 
+// Sign up API 
 app.post("/signup", async(req, res) => {
 
     // creating a new instance of the user model
@@ -20,6 +21,47 @@ app.post("/signup", async(req, res) => {
     }
     
 })
+
+
+// Get user by email
+app.get("/user", async (req, res) => {
+    const userEmail = req.body.emailId;
+
+    try{    
+      const user = await User.findOne({emailId: userEmail})
+      if(!user) {
+        res.status(400).send("cannot find user with this email Id")
+      }
+      else{
+        res.send(user);
+      }
+      
+    }
+    catch(err) {
+        res.status(400).send("something went wrong ")
+    }
+})
+
+
+
+
+// Feed API - GET /feed - get all the users from the database
+
+app.get("/feed", async (req, res) => {
+    try{
+        const users = await User.find({});
+        res.send(users);
+    }
+    catch(err) {
+        res.status(400).send("something went wrong ")
+    }
+
+})
+
+
+
+
+
 
 
 connectDB().then(() => {
