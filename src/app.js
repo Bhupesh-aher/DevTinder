23fe6b9ad2c7ba409ff6a8fbf50bfc70e49ede7e
Bhupesh-aher <aherbhupesh18@gmail.com>
2024-12-13 15:39:23 +0530

    ep6 created a database

--- src/app.js
+++ src/app.js
@@ -1,37 +1,41 @@
 const express = require("express")
 
 const app = express();
+const User = require('./models/user')
+const connectDB = require("./config/database")
 
+app.post("/signup", async(req, res) => {
 
+    const user = new User({
+        firstName: "deepika",
+        lastName: "padukon",
+        emailId: "deepika@padukom.com",
+        password: "deepika@123",
+    });
 
-app.get("/user", (req, res) => {
     try{
-        const flag =false; 
-        // here write logic about DB call and other and get the data 
-        // if the DB get the data then it will go to else conditon 
-        // otherwise it will go to if condition and it will throght an error
-        if(!flag){
-            // if from DB we doesn't get the data then just throw an error
-            throw new error("err") // this will go to catch stament and will throgh an error
-        }
-        else{
-            // here just send the data back to client
-            res.send("data")
-        }
-        
-        
-    }catch(err) {
-        res.status(500).send("some error contact support team")
-        
+        await user.save();
+        res.send("User added successfully!")
     }
+    catch(err){
+        res.status(400).send("Error saving the user:" + err.message);
+    }
+    
 })
 
 
+connectDB().then(() => {
+    console.log("database connection established..");
+    app.listen(7777, () => {
+        console.log("server is successfully listening on port 7777...");
+    });
+    
+})
+.catch(() => {
+    console.error("databse connot be connected!")
+})
 
 
-app.listen(7777, () => {
-    console.log("server is successfully listening on port 7777...");
-});
 
 
 
