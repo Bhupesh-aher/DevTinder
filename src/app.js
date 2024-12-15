75998dbc0908e766bcb9963998b7b3fa5c6d4998
Bhupesh-aher <aherbhupesh18@gmail.com>
2024-12-15 19:53:00 +0530

    added API level validation/sanitization on patch request

--- src/app.js
+++ src/app.js
@@ -72,16 +72,29 @@ app.delete("/user", async(req, res) => {
 
  
 // update data of the user
-app.patch("/user",async (req, res) => {
-    const userId = req.body.userId;
+app.patch("/user/:userId",async (req, res) => {
+    
+    // const userId = req.body.userId;
+    const userId = req.params?.userId;
     const data = req.body;
     
+
     try{
+        const ALLOWED_UPDATES = ["photUrl", "about", "gender", "age", "skills"]
+        const isUpdateAllowed = Object.keys(data).every((k) => ALLOWED_UPDATES.includes(k));
+
+        if(!isUpdateAllowed){
+            throw new Error("Update not allowed")
+        }
+        if(data?.skills.length > 10){
+            throw new Error("Skills cannot be more than 10")
+        }
+
         await User.findByIdAndUpdate({_id: userId}, data, {runValidators: true,})   
         res.send("user updated successfully")
-    }
+    }   
     catch(err) {
-        res.send("Update Failed:" + err.message)
+        res.send("Update Failed : " + err.message)
     }
 })
 
