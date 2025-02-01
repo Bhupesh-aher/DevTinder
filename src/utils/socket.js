3833aae283af30067df6f17f66307a5aca77ab8a
Bhupesh-aher <aherbhupesh18@gmail.com>
2025-02-01 19:10:05 +0530

    socket events & chat schema

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,5 +1,12 @@
 const socket = require("socket.io")
+const crypto = require('crypto')
 
+const getSecretRoomId = (userId, targetUserId) => {
+   return crypto
+        .createHash("sha256")
+        .update([userId, targetUserId].sort().join("_"))
+        .digest("hex");
+}
 
 const initializeSocket = (server) => {
 
@@ -11,12 +18,24 @@ const initializeSocket = (server) => {
 
     io.on("connection", (socket) => {
         
-        socket.on("joinChat", () => {
+        socket.on("joinChat", ({firstName, userId, targetUserId}) => {
 
+            const roomId = getSecretRoomId(userId, targetUserId)
+
+            // console.log( firstName + " " + "joining room : "   + roomId);
+            
+            socket.join(roomId)
         });
 
-        socket.on('sendMessage', () => {
+        socket.on('sendMessage', ({firstName ,userId, targetUserId, text }) => {
 
+            // whatever message i have got, i have  to send that to the particular room
+
+            const roomId = getSecretRoomId(userId, targetUserId)
+            // console.log(firstName + " " + text);
+            
+            // we transfer the message to the room
+            io.to(roomId).emit("messageReceived", {firstName, text})
         });
 
 
